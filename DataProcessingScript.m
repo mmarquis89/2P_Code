364a0adcb702a92122b433fbb3e56054ea873d2a
mmarquis <mmarquis89@gmail.com>
2017-09-13 13:58:50 -0400

    Add archive integrity testing to system7zip

--- DataProcessingScript.m
+++ DataProcessingScript.m
@@ -1,12 +1,24 @@
 
 %%% CREATE ANATOMY STACKS --------------------------------------------------------------------------
 fileStr = '*stack_*';
-dirPath = 'D:\Dropbox (HMS)\2P Data\Imaging Data\2017_09_06';
-CreateAnatomyStack(dirPath, fileStr, []);
+parentDir = 'D:\Dropbox (HMS)\2P Data\Imaging Data\2017_09_06';
+CreateAnatomyStack(parentDir, fileStr, []);
+
+%%% Archive raw anatomy stacks
+parentDir = 'D:\Dropbox (HMS)\2P Data\Imaging Data\2017_07_30\ZippedAnatomyStacks_stack';
+archiveName = 'AnatomyStacks';
+filterString = '*stack*';
+system7zip(parentDir, archiveName, '7z', filterString)
 
 %%% PRE-REGISTRATION PROCESSING --------------------------------------------------------------------
 parentDir = 'D:\Dropbox (HMS)\2P Data\Imaging Data\2017_09_06';
 preRegRoutine_MM(parentDir);
+
+%%% Archive raw imaging data files
+parentDir = 'D:\Dropbox (HMS)\2P Data\Imaging Data\2017_07_30\TrialData_sid_0+5_OldNames';
+archiveName = 'testArchive';
+filterString = '*sid_0_t*';
+system7zip(parentDir, archiveName, '7z', filterString, 1)
 
 %%% REGISTRATION -----------------------------------------------------------------------------------
 refVol = 6;
@@ -20,10 +32,10 @@ frameRate = 25;
 parentDir = 'D:\Dropbox (HMS)\2P Data\Behavior Vids\2017_09_06';
 makeVids_2P(0, parentDir, frameRate)
 
-%%% Zip raw video frames
-parentDir = 'D:\Dropbox (HMS)\2P Data\Behavior Vids\2017_07_30'
-archiveName = 'sid_5_RawFrames';
-test = system7zip(parentDir, archiveName, '7z', '*sid_5_tid*');
+%%% Archive raw video frames
+parentDir = 'D:\Dropbox (HMS)\2P Data\Behavior Vids\2017_09_06';
+archiveName = 'sid_0_RawFrames';
+system7zip(parentDir, archiveName, '7z', '*sid_0_t*', 1);
 
 %%% Concatenate vids
 parentDir = 'D:\Dropbox (HMS)\2P Data\Behavior Vids\2017_09_06\_Movies';
--- Deprecated/zipRawFrames.m
+++ Deprecated/zipRawFrames.m
@@ -1,32 +1,23 @@
-function archiveRawFrames(sid, parentDir)
-%==========================================================================================================
-% ADD RAW VIDEO FRAMES TO A COMPRESSED ARCHIVE
-% Calls 7zip from the Windows command line to archive raw video file
+function zipRawFrames(sid, parentDir)
+%============================================================================================================================
+% ADD RAW VIDEO FRAMES TO A ZIPPED ARCHIVE
 %
 % Inputs:
 %       sid = the session ID of the videos you want to process
 %       parentDir = the directory containing the raw data for the video frames you want to combine
 %               e.g. 'U:\2P Behavior Video\2017_07_30\_Movies'
-%==========================================================================================================
+%============================================================================================================================
 
-    % Make sure this .7z file doesn't already exist
+    % Make sure this zip file doesn't already exist
     zipFileName = ['sid_', num2str(sid), '_RawFrames'];
-    assert(exist(fullfile(parentDir, [zipFileName, '.7z']), 'file')==0, 'Error: a .7z file with that name already exists in this directory');
+    assert(exist(fullfile(parentDir, [zipFileName, '.zip']), 'file')==0, 'Error: a zip file with that name already exists in this directory');
 
     % Zip images
     tic
     disp('Zipping raw video data...');
-    
-    cmdStr_1 = 'D: & ' % Switch to the data drive
-    cmdStr_2 = ['cd ', parentDir, ' & '] % Set working directory to parentDir
-    cmdStr_3 = ['7z a sid_', num2str(sid), '_RawFrames.7z
-    system(['cd ', parentDir]);
-    
     sessionDirs = dir(fullfile(parentDir, ['*sid_', num2str(sid), '_tid*']));
     zipPaths = strcat([parentDir, '\'], {sessionDirs.name});
-    
     zip(fullfile(parentDir, zipFileName), zipPaths);
-    
     disp(['Zipping complete. Duration = ', num2str(toc), ' sec'])
 
 %     % Delete raw video frames if zipping was successful
